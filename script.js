8fecbd0e77764aa6620d6a992c2d835b7cc913c1
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-04 15:06:13 +0200

    test

--- script.js
+++ script.js
@@ -3,6 +3,24 @@
 */
 
 
+// prove for each
 
-console.log('ciao')
+const cose = [
+  {nome: 'matita',colore:'nero'},
+  {nome: 'penna',colore:'blu'},
+  {nome: 'pennarelloR',colore:'rosso'},
+  {nome: 'pennarelloB',colore:'nero'},
+];
+
+// map test
+const cosi = cose.map((coso)=>{
+  return coso.nome;
+})
+console.log('cosi nome dei singoli oggetti distribuiti in nuovo array :',cosi)
+console.log('cose array orig :',cose)
+
+// foreach test
+// cose.forEach((element, index, array) => {
+//   console.log(element, index, )
+// })
 
