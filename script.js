6423f4d2571c78e46f4a0a02e3b0fa62c1d34ea0
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-04 15:36:56 +0200

    init elenco utenti

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
 /*
-  1.
+  1.generare array di oggetti utenti
+  2.generare altro array con solo utenti over65
+  ----------
 */
 
+const utenti = [
+  {
+    nome : 'gianni',
+    cognome : 'rossi',
+    eta : 75,
+},
+  {
+    nome : 'maria',
+    cognome : 'verdi',
+    eta : 82,
+},
+  {
+    nome : 'mario',
+    cognome : 'bianchi',
+    eta : 45,
+},
+  {
+    nome : 'filippo',
+    cognome : 'marrone',
+    eta : 25,
+},
+  {
+    nome : 'giovanni',
+    cognome : 'cip ciop',
+    eta : 19,
+},
 
-// prove for each
+]
 
-const cose = [
-  {nome: 'matita',colore:'nero'},
-  {nome: 'penna',colore:'blu'},
-  {nome: 'pennarelloR',colore:'rosso'},
-  {nome: 'pennarelloB',colore:'nero'},
-];
 
-// filter test
-const neri = cose.filter((coso)=>{
-  if (coso.colore === 'nero'){
-    return coso;
-  }
-});
-console.log('neri',neri)
 
-// map test
-const cosi = cose.map((coso)=>{
-  return coso.nome;
-})
-console.log('cosi nome dei singoli oggetti distribuiti in nuovo array :',cosi)
-console.log('cose array orig :',cose)
+
+
+
+
+
+// prove for each------------------------------------------
+
+// const cose = [
+//   {nome: 'matita',colore:'nero'},
+//   {nome: 'penna',colore:'blu'},
+//   {nome: 'pennarelloR',colore:'rosso'},
+//   {nome: 'pennarelloB',colore:'nero'},
+// ];
+
+// // filter test
+// const neri = cose.filter((coso)=>{
+//   if (coso.colore === 'nero'){
+//     return coso;
+//   }
+// });
+// console.log('neri',neri)
+
+// // map test
+// const cosi = cose.map((coso)=>{
+//   return coso.nome;
+// })
+// console.log('cosi nome dei singoli oggetti distribuiti in nuovo array :',cosi)
+// console.log('cose array orig :',cose)
 
 
 // foreach test
