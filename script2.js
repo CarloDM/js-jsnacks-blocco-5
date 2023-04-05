c826baf095e66a5944a9857da86901279563da28
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-05 15:09:16 +0200

    snk5

--- script2.js
+++ script2.js
@@ -17,11 +17,22 @@ studenti.forEach((studente) => nameList.push((studente.nome).toUpperCase()))
 console.log('nomi per targhette',nameList);
 
 // lista stud con voto superiore a 70
-let over70List = [studenti.filter((studente)=>{if (studente.voto > 69) return studente})]
+let over70List = [studenti.filter((studente) => {if (studente.voto > 69) return studente})]
 console.log('lista voto sopra 70',over70List);
 
 // lista stud con voto superiore a 70 e id superiore a 120
-let over70Over120Id = [studenti.filter((studente)=>{if (studente.voto >= 70 && studente.id >= 120) return studente})]
+let over70Over120Id = [studenti.filter((studente) => {if (studente.voto >= 70 && studente.id >= 120) return studente})]
 console.log('lista over 70 e id over 120',over70Over120Id);
 
 // <--------------------------------------------------------------
+
+// snk 5
+// lista bici da corsa nome e peso
+const bici = [
+  {nome: 'cannondale', peso : 8.5},
+  {nome: 'bianchi', peso : 8},
+  {nome: 'specialized', peso : 10},
+  {nome: 'stucchi', peso : 9.5},
+  {nome: 'locomotiv', peso : 9}
+]
+
